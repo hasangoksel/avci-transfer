c4f625d891c6c21ee88b86ed7df3517806425395
Hasan Göksel <hgoksel19@posta.pau.edu.tr>
2024-05-03 01:48:35 +0300

    Created: Contact Form Client & Created: Contact Model & Migration & Controller

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,15 @@
-<script setup>
-</script>
-
 <template>
-  
+  <contact-form></contact-form>
 </template>
+<script>
+import ContactForm from '@/components/ContactForm.vue';
 
+export default {
+  components: {
+    "contact-form": ContactForm
+  },
+};
+</script>
 <style scoped>
 
 </style>
