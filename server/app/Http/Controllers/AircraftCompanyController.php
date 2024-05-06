6391a264559350c2d0ad90ef256279676bbb0215
Berkay Özenel <berkayozenel1@gmail.com>
2024-05-06 18:13:53 +0300

    Vehicle Type table and Aircraft Company table are created, migrated and added 4-5 datas.

---
+++ server/app/Http/Controllers/AircraftCompanyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AircraftCompany;
+use Illuminate\Http\Request;
+
+class AircraftCompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AircraftCompany $aircraftCompany)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AircraftCompany $aircraftCompany)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AircraftCompany $aircraftCompany)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AircraftCompany $aircraftCompany)
+    {
+        //
+    }
+}
