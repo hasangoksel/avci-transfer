3479330d6aeac04b16382233c138f025fe312917
Berkay Özenel <berkayozenel1@gmail.com>
2024-05-10 01:40:36 +0300

    Update: Relationships

--- server/app/Models/Reservation.php
+++ server/app/Models/Reservation.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use App\Models\Service;
+use App\Models\VehicleType;
+use App\Models\AircraftCompany;
 
 class Reservation extends Model
 {
     use HasFactory;
+    /**
+     * Get the user associated with the Reservation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function services(): HasOne
+    {
+        return $this->hasOne(Service::class, 'servicesID', 'servicesID');
+    }
+
+    /**
+     * Get the user associated with the Reservation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function vehicleType(): HasOne
+    {
+        return $this->hasOne(VehicleType::class, 'vehicleTypeID', 'vehicleTypeID');
+    }
+
+    /**
+     * Get the user associated with the Reservation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function company(): HasOne
+    {
+        return $this->hasOne(AircraftCompany::class, 'companyID', 'companyID');
+    }
 }
