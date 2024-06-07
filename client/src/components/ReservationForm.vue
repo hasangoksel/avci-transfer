679d5040b5b18741f7fb9af6a75a1b5da14a9b41
Hasan Göksel <hgoksel19@posta.pau.edu.tr>
2024-06-07 10:57:28 +0300

    Update

--- client/src/components/ReservationForm.vue
+++ client/src/components/ReservationForm.vue
@@ -1,29 +1,52 @@
 <template>
     <div class="reservation-form-container">
         <div class="reservation-form-nav">
-            <div class="reservation-form-nav__stage-one"></div>
-            <div class="reservation-form-nav__stage-two"></div>
-            <div class="reservation-form-nav__stage-three"></div>
-            <div class="reservation-form-nav__stage-four"></div>
-            <div class="reservation-form-nav__stage-five"></div>
+            <div class="reservation-form-nav__stage">
+                <i class="fa-solid fa-arrow-right-arrow-left"></i>
+                <span>Rezervasyon Bilgileri</span>
+            </div>
+            <div class="reservation-form-nav__stage">
+                <i class="fa-solid fa-car"></i>
+                <span>Transfer Seçimi</span>
+            </div>
+            <div class="reservation-form-nav__stage">
+                <i class="fa-solid fa-user"></i>
+                <span>Yolcu Bilgileri</span>
+            </div>
+            <div class="reservation-form-nav__stage">
+                <i class="fa-solid fa-plane"></i>
+                <span>Uçuş Bilgileri</span>
+            </div>
+            <div class="reservation-form-nav__stage">
+                <i class="fa-solid fa-check-double"></i>
+                <span>Rezervasyon Onayı</span>
+            </div>
         </div>
         <form class="reservation-form">
             <div class="reservation-form__data">
-                <div v-if="cOne" class="reservation-form__data__stage-one">
+                <div v-if="cOne" class="reservation-form__data__stage">
                     <h3>Rezervasyon Bilgileri</h3>
                     <div class="reservation-form__element">
-                        <label>Transfer Tipi</label><br>
-                        <input type="radio" id="shared" v-model="transferType" name="transferType" value="Paylaşımlı">
-                        <label for="shared">Paylaşımlı</label><br>
-                        <input type="radio" id="special" v-model="transferType" name="transferType" value="Özel">
+                        <label>Transfer Tipi</label>
+                        <div>
+                            <input type="radio" id="shared" v-model="transferType" name="transferType" value="Paylaşımlı">
+                            <label for="shared">Paylaşımlı</label><br>
+                        </div>
+                        <div>
+                            <input type="radio" id="special" v-model="transferType" name="transferType" value="Özel">
                         <label for="special">Özel</label><br>
+                        </div>
                     </div>
                     <div class="reservation-form__element">
-                        <label>Yön Tercihi</label><br>
-                        <input type="radio" id="airportToArea" v-model="direction" name="direction" value="airportToArea">
-                        <label for="airportToArea">Havaalanından Bölgeye</label><br>
-                        <input type="radio" id="areaToAirport" v-model="direction" name="direction" value="areaToAirport">
-                        <label for="areaToAirport">Bölgeden Havaalanına</label><br>
+                        <label>Yön Tercihi</label>
+                        <div>
+                            <input type="radio" id="airportToArea" v-model="direction" name="direction" value="airportToArea">
+                            <label for="airportToArea">Havaalanından Bölgeye</label><br>
+                        </div>
+                        <div>
+                            <input type="radio" id="areaToAirport" v-model="direction" name="direction" value="areaToAirport">
+                            <label for="areaToAirport">Bölgeden Havaalanına</label><br>
+                        </div>
                     </div>
                     <div class="reservation-form__element">
                         <label for="date">Tarih</label>
@@ -48,9 +71,11 @@
                         <label for="count">Kişi Sayısı</label>
                         <input type="number" id="count" name="count" v-model="count" min="1" max="10">
                     </div>
-                    <span class="c-button" @click="cOneClick">Devam Et</span>
+                    <div class="reservation-form__button">
+                        <span @click="cOneClick">Devam Et</span>
+                    </div>
                 </div>
-                <div v-if="cTwo" class="reservation-form__data__stage-two">
+                <div v-if="cTwo" class="reservation-form__data__stage">
                     <h3>Transfer Seçimi</h3>
                     <div class="reservation-form__element__vehicleType" v-for="item in vehicleType" :key="item.vehicleTypeID">
                         <input type="radio" :id="item.vehicleTypeID" name="vehicle" v-model="reservationData.vehicle" :value="item.type">
@@ -60,9 +85,11 @@
                             <span class="vehicle-type__price">{{ price * count }}</span>
                         </label>
                     </div>
-                    <span class="c-button" @click="cTwoClick">Devam Et</span>
+                    <div class="reservation-form__button">
+                        <span class="c-button" @click="cTwoClick">Devam Et</span>
+                    </div>
                 </div>
-                <div v-if="cThree" class="reservation-form__data__stage-three">
+                <div v-if="cThree" class="reservation-form__data__stage">
                     <h3>Yolcu Bilgileri</h3>
                     <div class="travellerData" v-for="(traveller, index) in travellers" :key="index">
                         <h4>Yolcu - {{ index + 1 }}</h4>
@@ -91,9 +118,11 @@
                             <input type="text" :id="'address-' + index" v-model="traveller.address" />
                         </div>
                     </div>
-                    <span class="c-button" @click="cThreeClick">Devam Et</span>
+                    <div class="reservation-form__button">
+                        <span class="c-button" @click="cThreeClick">Devam Et</span>
+                    </div>
                 </div>
-                <div v-if="cFour" class="reservation-form__data__stage-four">
+                <div v-if="cFour" class="reservation-form__data__stage">
                     <h3>Transfer Bilgileri</h3>
                     <div class="reservation-form__element">
                         <label for="airlineCompany">Havayolu Şirketi</label>
@@ -110,16 +139,20 @@
                         <label for="flightTime">Uçuş Saati</label>
                         <input type="time" name="flightTime" id="flightTime" v-model="reservationData.flightTime">
                     </div>
-                    <span class="c-button" @click="cFourClick">Devam Et</span>
+                    <div class="reservation-form__button">
+                        <span class="c-button" @click="cFourClick">Devam Et</span>
+                    </div>
                 </div>
-                <div v-if="cFive" class="reservation-form__data__stage-five">
+                <div v-if="cFive" class="reservation-form__data__stage">
                     <h3>Rezervasyon Onayı</h3>
                     <div class="reservation-form__element">
                         <label for="confirmation">Lütfen +90 (5***) *** *1 11 telefona gelen onay kodunu giriniz:
                         </label>
                         <input type="number" name="confirmation" id="confirmation">
                     </div>
-                    <span class="c-button" @click="cFiveClick">Onayla</span>
+                    <div class="reservation-form__button">
+                        <span class="c-button" @click="cFiveClick">Onayla</span>
+                    </div>
                 </div>
             </div>
             <div class="reservation-form__preview"></div>
@@ -263,3 +296,82 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+.reservation-form-nav{
+    width: 80%;
+    margin: 0 10%;
+    display: flex;
+    justify-content: space-between ;
+    align-items: center;
+}
+.reservation-form-nav__stage{
+    height: 4rem;
+    display: flex;
+    align-items: center;
+}
+.reservation-form-nav__stage i{
+    font-size: 1.6rem;
+    margin-right: 12px;
+    color: #ddbc04;
+}
+.reservation-form{
+    width: 80%;
+    margin: 3% 10%;
+    display: flex;
+    justify-content: space-between; 
+}
+.reservation-form__data{
+    width: 50%;
+}
+.reservation-form__element{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-top: 1%;
+}
+.reservation-form__element input{
+    width: 50%;
+    height: 2rem;
+    font-size: 1rem;
+    border-radius: 5px;
+    box-shadow: none;
+    padding-left: 1%;
+    padding-right: 1%;
+    border: 1px solid gray;
+}
+.reservation-form__element select{
+    width: 50%;
+    height: 2rem;
+    font-size: 1rem;
+    border-radius: 5px;
+    padding-left: 1%;
+    padding-right: 1%;
+}
+.reservation-form__element div input{
+    width: 20px;
+    margin-right: .28rem;
+    height: 1rem;
+    font-size: 1rem;
+}
+.reservation-form__button{
+    margin-top: 3%;
+    width: 50%;
+    display: flex;
+    justify-content: end;
+}
+.reservation-form__button span{
+    border: 1px solid #000;
+    padding: 1.8% 5%;
+    border-radius: 5px;
+    background: #000;
+    color: #ddbc04;
+    transition: background .2s ease;
+}
+.reservation-form__button span:hover{
+    cursor: pointer;
+    background: #ddbc04;
+    color: black;
+    border-color: #ddbc04;
+}
+</style>
